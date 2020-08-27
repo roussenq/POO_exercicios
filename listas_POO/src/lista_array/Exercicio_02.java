efd9c22206b76c42239322711ddbf6e91d0c14ac
David <davidroussenq@gmail.com>
2020-08-26 23:59:49 -0300

    Update Exercicio_02.java

--- listas_POO/src/lista_array/Exercicio_02.java
+++ listas_POO/src/lista_array/Exercicio_02.java
@@ -1,1 +1,21 @@
-2-CRIE UM ARRAY DE INTEIROS DETAMANHO 10.IMPLEMENTE UM PROGRAMA QUE DEFINA E ESCREVA O ARRAY,BEM COMO A SOMA DOSELEMENTOS ARMAZENADOS NESTEARRAY.
+/**
+ * 
+ */
+package lista_array;
+
+/**
+ * @author David Roussenq Maria
+ * Curso ADS - Senac 2020-1
+ */
+public class Exercicio_02 {
+
+	/**
+	 * 2-CRIE UM ARRAY DE INTEIROS DETAMANHO 10.
+   * IMPLEMENTE UM PROGRAMA QUE DEFINA E ESCREVA O ARRAY,
+   * BEM COMO A SOMA DOSELEMENTOS ARMAZENADOS NESTEARRAY.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}

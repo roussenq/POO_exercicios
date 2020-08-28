be3d23dbe3e11f66aeaa92508b606401f68754ce
roussenq <davidroussenq@gmail.com>
2020-08-28 00:39:18 -0300

    modulo criado para intellij

--- listas_POO/src/lista_array/Exercicio_01.java
+++ listas_POO/src/lista_array/Exercicio_01.java
@@ -1,34 +1,32 @@
 package lista_array;
 
 import java.util.Random;
-
 /**
  * @author David Roussenq Maria
- * Curso ADS - Senac 2020-1
+ * Curso ADS - Senac 2020-2
  */
 public class Exercicio_01 {
+    /**
+     * 1-CRIE UM ARRAY DE INTEIROS DETAMANHO 10.
+     * IMPLEMENTE UM PROGRAMA QUE DEFINA E ESCREVA O ARRAY,
+     * BEM COMO A QUANTIDADE DE ELEMENTOS ARMAZENADOS NESTE ARRAY QUE Sï¿½O PARES.
+     */
+    public static void main(String[] args) {
 
-	/**
-	 * 1-CRIE UM ARRAY DE INTEIROS DETAMANHO 10.
-	 * IMPLEMENTE UM PROGRAMA QUE DEFINA E ESCREVA O ARRAY,
-	 * BEM COMO A QUANTIDADE DE ELEMENTOS ARMAZENADOS NESTE ARRAY QUE SÃO PARES.
-	 */
-	public static void main(String[] args) {
-		
-		Random gerador = new Random();
-		int[] numeros = new int[10];
-		int contador = 0;
-		
-		for (int i = 0; i < numeros.length;i++) {
-			numeros[i]= gerador.nextInt(9) + 1;
-			System.out.println(numeros[i]);
-			if (numeros[i] % 2 == 0) {
-				contador++;
-			}
-		}
-		
-		System.out.println("Total de nº pares: "+contador);
+        Random gerador = new Random();
+        int[] numeros = new int[10];
+        int contador = 0;
 
-	}
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = gerador.nextInt(9) + 1;
+            System.out.println(numeros[i]);
+            if (numeros[i] % 2 == 0) {
+                contador++;
+            }
+        }
+
+        System.out.println("Total de numeros pares: " + contador);
+
+    }
 
 }
--- listas_POO/src/lista_array/Exercicio_02.java
+++ listas_POO/src/lista_array/Exercicio_02.java
@@ -7,22 +7,22 @@ import java.util.Random;
  */
 public class Exercicio_02 {
 
-	/**
-	 * 2-CRIE UM ARRAY DE INTEIROS DETAMANHO 10. IMPLEMENTE UM PROGRAMA QUE DEFINA E
-	 * ESCREVA O ARRAY, BEM COMO A SOMA DOS ELEMENTOS ARMAZENADOS NESTEARRAY.
-	 */
-	public static void main(String[] args) {
+    /**
+     * 2-CRIE UM ARRAY DE INTEIROS DETAMANHO 10. IMPLEMENTE UM PROGRAMA QUE DEFINA E
+     * ESCREVA O ARRAY, BEM COMO A SOMA DOS ELEMENTOS ARMAZENADOS NESTEARRAY.
+     */
+    public static void main(String[] args) {
 
-		Random gerador = new Random();
-		int[] numeros = new int[10];
-		int soma = 0;
+        Random gerador = new Random();
+        int[] numeros = new int[10];
+        int soma = 0;
 
-		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = gerador.nextInt(9) + 1;
-			soma += numeros[i];
-			System.out.println("Elemento nº" + (i + 1) + ": " + numeros[i]);
-		}
-		System.out.println("A soma dos elementos do array é: " + soma);
-	}
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = gerador.nextInt(9) + 1;
+            soma += numeros[i];
+            System.out.println("Elemento nï¿½" + (i + 1) + ": " + numeros[i]);
+        }
+        System.out.println("A soma dos elementos do array ï¿½: " + soma);
+    }
 
 }
